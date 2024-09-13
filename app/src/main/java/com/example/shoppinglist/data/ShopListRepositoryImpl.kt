b0a826e8c834439f202850274b2a8c8d9af63e1c
Egor Lazarev <finta3230@gmail.com>
2024-09-13 16:38:35 +0300

    Lesson 3.6 fixed

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -15,17 +15,17 @@ object ShopListRepositoryImpl : ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
 
-//    private var autoIncrementId = 0
+    private var autoIncrementId = 0
 
-//    init {
-//        for (i in 0 until 10){
-//            val item = ShopItem("Name $i", i, true)
-//            addShopItem(item)
-//        }
-//    }
+    init {
+        for (i in 0 until 10){
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(item: ShopItem) {
-//        if (item.id == ShopItem.UNDEFINED_ID) item.id = autoIncrementId++
+        if (item.id == ShopItem.UNDEFINED_ID) item.id = autoIncrementId++
         shopList.add(item)
         updateList()
     }
